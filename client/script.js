c7bdd6f5b7480576cf2a7acb96392001bb7d3c37
Vlad <lgbtqtoken366@gmail.com>
2024-12-31 12:57:08 +0100

    +

--- client/script.js
+++ client/script.js
@@ -173,9 +173,7 @@ function createTokenPanel(token) {
                 <span class="token-name">${token.name}</span>
             </div>
             <span class="token-price">$${round(token.price,2)}
-            <span class="price-arrow" id="arrow-${token.symbol}" data-symbol="${token.symbol}">
-                    ${''}
-                </span>
+            <span class="price-arrow" id="arrow-${token.symbol}" data-symbol="${token.symbol}"> ${''} </span>
             </span>
         </div>
     </div>
