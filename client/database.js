173142cd19b1bb2167b0a9f095d9471da2cc2f53
Vlad <lgbtqtoken366@gmail.com>
2025-01-01 14:25:37 +0100

    +

--- client/database.js
+++ client/database.js
@@ -1,8 +1,8 @@
 export async function getWalletData(wallet_address) {
     try {
         console.log('Getting active wallet for wallet', wallet_address);
-        // const response = await fetch(`https://miniappserv.com/api/wallets/data/${wallet_address}`);
-        const response = await fetch(`https://miniappserv.com/api/wallets/data/GBQCR3L7H2QBCJNEI3CLBRCGQFSTGPEPRW3U2NPQRUJ66ZVQ7SECSUHQ`);
+        const response = await fetch(`https://miniappserv.com/api/wallets/data/${wallet_address}`);
+        // const response = await fetch(`https://miniappserv.com/api/wallets/data/GBQCR3L7H2QBCJNEI3CLBRCGQFSTGPEPRW3U2NPQRUJ66ZVQ7SECSUHQ`);
         if (!response.ok) {
             throw new Error(`Error: ${response.statusText}`);
         }
